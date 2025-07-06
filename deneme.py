4669fac90352b04dd3c026133057e70227e6dfd7
Enko Zukovic <enis.buyukbayrak01@gmail.com>
2025-07-06 11:57:27 +0300

    yorum eklendi yeni

--- deneme.py
+++ deneme.py
@@ -3,7 +3,7 @@ import tkinter as Tk
 from tkinter.ttk import Combobox
 from PIL import ImageTk, Image
 #Kamera seçimi yapılıyor
-#yorum eklendi
+#yorum eklendi.
 cap = cv.VideoCapture(0)
 
 class LIHAIHAYerIstasyonu:
